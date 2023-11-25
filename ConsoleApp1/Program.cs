9873e52dc919de6521167e24210b40cfa5dc006b
Lucas <giovannilucas012@gmail.com>
2023-11-25 14:28:27 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string datastring = "2021-16-26 14:19";
+
+            DateTime.TryParseExact(datastring, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
+            Console.WriteLine(data);
+            Console.ReadLine();
+        }
+    }
+}
